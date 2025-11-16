4c9c618bb5b53715300cc4a7b14fc6ec62fafc83
Oliver Fitzgerald <ofitzgerald15@gmail.com>
2025-11-16 20:25:05 +0000

    Deleting doc directory using gh-pages instead

--- src/App.tsx
+++ src/App.tsx
@@ -133,7 +133,7 @@ function InstagramIcon() {
 
 export default function App() {
   return (
-    <BrowserRouter basename="/roisin">
+    <BrowserRouter basename="/github-pages-test">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/artist-statement" element={<ArtistStatement />} />
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client';
 import './main.css';
 import App from './App.tsx';
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <App />
   </StrictMode>
 );
